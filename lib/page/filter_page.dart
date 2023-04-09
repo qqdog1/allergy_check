d721a289eaece4a1ec2639f3a97e641e6c6bb894
Shawn <chenhsiangchou@gmail.com>
2023-04-09 22:58:47 +0800

    每一頁都加一個說明popup

--- lib/page/filter_page.dart
+++ lib/page/filter_page.dart
@@ -8,6 +8,34 @@ class FilterPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('搜尋過敏原'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showPopup(context);
+            },
+            icon: const Icon(Icons.question_mark_sharp),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> showPopup(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => AlertDialog(
+        content: const Text('這裡可以設定已知過敏原，\n'
+            '複雜設定:\n'
+            '一次帶入APP預設200多項過敏原\n'
+            '簡易設定:\n'
+            '自行逐一新增過敏原'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('確定'),
+          ),
+        ],
       ),
     );
   }
--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -1,13 +1,69 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class SettingsPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  _SettingsPage createState() => _SettingsPage();
+}
+
+class _SettingsPage extends State<SettingsPage>
+    with SingleTickerProviderStateMixin {
+  bool isSet = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initSharedPreferences();
+  }
+
+  Future<void> initSharedPreferences() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isSet = prefs.getBool('isSet') ?? false;
+      if (!isSet) {
+        showPopup(context);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('設定已知過敏原'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showPopup(context);
+            },
+            icon: const Icon(Icons.question_mark_sharp),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> showPopup(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => AlertDialog(
+        content: const Text('這裡可以設定已知過敏原，\n'
+            '複雜設定:\n'
+            '一次帶入APP預設200多項過敏原\n'
+            '簡易設定:\n'
+            '自行逐一新增過敏原'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('確定'),
+          ),
+        ],
       ),
     );
   }
